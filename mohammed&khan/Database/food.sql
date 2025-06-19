7c163bc46ee803b04602d7648408cc655fd91860
Mohammedaman6901 <mohammedaman6901@gmail.com>
2025-06-19 11:33:03 +0530

    commit
    major push

---
+++ mohammed&khan/Database/food.sql
@@ -0,0 +1,244 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: June 25, 2024 at 02:46 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: 'food'
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `username` varchar(30) NOT NULL,
+  `fullname` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `contact` varchar(30) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`username`, `fullname`, `email`, `contact`, `address`, `password`) VALUES
+('arbaaz9845', 'Arbaaz', 'arbaazkhan9845@gmail.com', '7411728543', 'banglore', '9845'),
+('chetu123', 'Chetana', 'chetu@gmail.cm', '7865955856', 'shimoga', '6789'),
+('nishu123', 'Nishad anjum', 'nishadanjum17@gmail.com', '1234567890', 'Davangere', '5467'),
+('padma123', 'padmavathi', 'padma@gmail.com', '9342345567', 'Bhadravathi', '9108');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `food`
+--
+
+CREATE TABLE `food` (
+  `F_ID` int(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `price` int(30) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `R_ID` int(30) NOT NULL,
+  `images_path` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `food`
+--
+
+INSERT INTO `food` (`F_ID`, `name`, `price`, `description`, `R_ID`, `images_path`) VALUES
+(1, 'brownie beetroot', 200, 'Yummy', 1, 'images/p5.jpg'),
+(2, 'DOSA', 120, 'TASTY', 1, 'images/p13.jpg'),
+(3, 'Noodles', 180, 'amazing', 1, 'images/p21.jpg'),
+(4, 'cake rolls', 200, 'sweet and yummy', 1, 'images/gc.jpg'),
+(5, 'Pani puri', 80, 'Yummy', 1, 'images/p8.jpg'),
+(6, 'masala meal', 180, 'amazing', 1, 'images/masala.jpg'),
+(7, 'Black Coco', 70, 'ahh yummy', 1, 'images/mg.jpg'),
+(8, 'cake', 200, 'TASTY', 1, 'images/p1.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manager`
+--
+
+CREATE TABLE `manager` (
+  `username` varchar(30) NOT NULL,
+  `fullname` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `contact` varchar(30) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `manager`
+--
+
+INSERT INTO `manager` (`username`, `fullname`, `email`, `contact`, `address`, `password`) VALUES
+('anjum123', 'nishad', 'nishadanjumpjncs@gmail.com', '8976543267', 'shimoga', '0099'),
+('arbaaz123', 'John', 'john123@gmail.com', '8976543267', 'Banglore', '0000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_ID` int(30) NOT NULL,
+  `F_ID` int(30) NOT NULL,
+  `foodname` varchar(30) NOT NULL,
+  `price` int(30) NOT NULL,
+  `quantity` int(30) NOT NULL,
+  `order_date` date NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `R_ID` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_ID`, `F_ID`, `foodname`, `price`, `quantity`, `order_date`, `username`, `R_ID`) VALUES
+(1, 1, 'brownie beetroot', 200, 1, '2021-01-11', 'nishu123', 1),
+(2, 3, 'Noodles', 180, 1, '2021-01-11', 'chetu123', 1),
+(3, 4, 'cake rolls', 200, 1, '2021-01-11', 'padma123', 1),
+(4, 6, 'masala meal', 180, 1, '2021-01-11', 'aishu123', 1);
+
+-- --------------------------------------------------------
+
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `restaurants`
+--
+
+CREATE TABLE `restaurants` (
+  `R_ID` int(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `contact` varchar(30) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `M_ID` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `restaurants`
+--
+
+INSERT INTO `restaurants` (`R_ID`, `name`, `email`, `contact`, `address`, `M_ID`) VALUES
+(1, 'Sea food restaurant', 'seafoodrestaurant@gmail.com', '9108669212', 'shimoga', 'john123');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `food`
+--
+ALTER TABLE `food`
+  ADD PRIMARY KEY (`F_ID`,`R_ID`),
+  ADD KEY `R_ID` (`R_ID`);
+
+--
+-- Indexes for table `manager`
+--
+ALTER TABLE `manager`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_ID`),
+  ADD KEY `F_ID` (`F_ID`),
+  ADD KEY `username` (`username`),
+  ADD KEY `R_ID` (`R_ID`);
+
+--
+-- Indexes for table `restaurants`
+--
+ALTER TABLE `restaurants`
+  ADD PRIMARY KEY (`R_ID`),
+  ADD UNIQUE KEY `M_ID_2` (`M_ID`),
+  ADD KEY `M_ID` (`M_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `food`
+--
+ALTER TABLE `food`
+  MODIFY `F_ID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_ID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `restaurants`
+--
+ALTER TABLE `restaurants`
+  MODIFY `R_ID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `food`
+--
+ALTER TABLE `food`
+  ADD CONSTRAINT `food_ibfk_1` FOREIGN KEY (`R_ID`) REFERENCES `restaurants` (`R_ID`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`F_ID`) REFERENCES `food` (`F_ID`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`username`) REFERENCES `customer` (`username`),
+  ADD CONSTRAINT `orders_ibfk_3` FOREIGN KEY (`R_ID`) REFERENCES `restaurants` (`R_ID`);
+
+--
+-- Constraints for table `restaurants`
+--
+ALTER TABLE `restaurants`
+  ADD CONSTRAINT `restaurants_ibfk_1` FOREIGN KEY (`M_ID`) REFERENCES `manager` (`username`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
