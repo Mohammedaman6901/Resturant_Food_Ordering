9b1e01ba540ddb978581e7e7b3fbc7b912bddea8
Mohammedaman6901 <mohammedaman6901@gmail.com>
2025-06-21 22:50:53 +0530

    Update index.php

--- mohammed&khan/index.php
+++ mohammed&khan/index.php
@@ -1,263 +1,166 @@
-<?php
-session_start();
-?>
-
+<!DOCTYPE html>
 <html>
+<head>
+  <title>Home | Sea Food Restaurant</title>
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
 
-  <head>
-    <title> Home | Sea Food Restaurant </title>
-  </head>
+<body>
 
-  <link rel="stylesheet" type = "text/css" href ="css/bootstrap.min.css">
+<!-- Back to top button -->
+<button onclick="topFunction()" id="myBtn" title="Go to top">
+  <span class="glyphicon glyphicon-chevron-up"></span>
+</button>
 
-  <link rel="stylesheet" type = "text/css" href ="css/index.css">
+<!-- JavaScript for back to top button -->
+<script type="text/javascript">
+  window.onscroll = function () {
+    scrollFunction()
+  };
 
-  <body>
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("myBtn").style.display = "block";
+    } else {
+      document.getElementById("myBtn").style.display = "none";
+    }
+  }
 
-  <!--Back to top button..................................................................................-->
-    <button onclick="topFunction()" id="myBtn" title="Go to top">
-      <span class="glyphicon glyphicon-chevron-up"></span>
-    </button>
-  <!--Javacript for back to top button....................................................................-->
-    <script type="text/javascript">
-      window.onscroll = function()
-      {
-        scrollFunction()
-      };
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+</script>
 
-      function scrollFunction(){
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-          document.getElementById("myBtn").style.display = "block";
-        } else {
-          document.getElementById("myBtn").style.display = "none";
-        }
-      }
-
-      function topFunction() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-      }
-    </script>
-
-    <nav class="navbar navbar-inverse navbar-fixed-top navigation-clean-search" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="index.php"> Sea Food Restaurant</a>
-        </div>
-
-        <div class="collapse navbar-collapse " id="myNavbar">
-          <ul class="nav navbar-nav">
-            <li class="active" ><a href="index.php">Home</a></li>
-            <li><a href="aboutus.php">About</a></li>
-            <li><a href="contactus.php">Contact Us</a></li>
-
-          </ul>
-
-<?php
-if(isset($_SESSION['login_user1'])){
-
-?>
-
-
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $_SESSION['login_user1']; ?> </a></li>
-            <li><a href="myrestaurant.php">MANAGER CONTROL PANEL</a></li>
-            <li><a href="logout_m.php"><span class="glyphicon glyphicon-log-out"></span> Log Out </a></li>
-          </ul>
-<?php
-}
-else if (isset($_SESSION['login_user2'])) {
-  ?>
-           <ul class="nav navbar-nav navbar-right">
-            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $_SESSION['login_user2']; ?> </a></li>
-            <li><a href="foodlist.php"><span class="glyphicon glyphicon-cutlery"></span> Food Zone </a></li>
-            <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span> Cart
-              (<?php
-              if(isset($_SESSION["cart"])){
-              $count = count($_SESSION["cart"]); 
-              echo "$count"; 
-            }
-              else
-                echo "0";
-              ?>)
-             </a></li>
-            <li><a href="logout_u.php"><span class="glyphicon glyphicon-log-out"></span> Log Out </a></li>
-          </ul>
-  <?php        
-}
-else {
-
-  ?>
-
-<ul class="nav navbar-nav navbar-right">
-            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span> Sign Up <span class="caret"></span> </a>
-                <ul class="dropdown-menu">
-              <li> <a href="customersignup.php"> User Sign-up</a></li>
-              <li> <a href="managersignup.php"> Manager Sign-up</a></li>
-              
-            </ul>
-            </li>
-
-            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-log-in"></span> Login <span class="caret"></span></a>
-              <ul class="dropdown-menu">
-              <li> <a href="customerlogin.php"> User Login</a></li>
-              <li> <a href="managerlogin.php"> Manager Login</a></li>
-             
-            </ul>
-            </li>
-          </ul>
-
-<?php
-}
-?>
-
-
-        </div>
-
-      </div>
-    </nav>
-
-    <div class="wide">
-      	<div class="col-xs-5 line"><hr></div>
-       
-        <div class="col-xs-5 line"><hr></div>
-        <div class="tagline"></div>
-    </div>
-    <br>
-    
-    <div class="orderblock">
-    <h2>Feeling Hungry?</h2>
-    <center><a class="btn btn-success btn-lg" href="customerlogin.php" role="button" > Order Now </a></center>
+<!-- Navigation Bar -->
+<nav class="navbar navbar-inverse navbar-fixed-top navigation-clean-search" role="navigation">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Sea Food Restaurant</a>
     </div>
 
-    <div class="col-xs-12 line"><hr></div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="aboutus.html">About</a></li>
+        <li><a href="contactus.html">Contact Us</a></li>
+      </ul>
 
-    <div class="wide2">
-        <div class="col-xs-4 box">
-          <img src="images/minimumx.png" height="200px">
-        </div>
-        <div class="col-xs-4 box">
-          <img src="images/locationx.png">
-        </div>
-        <div class="col-xs-4 box">
-          <img src="images/deliveryx.png">
-        </div>
-
-        <div class="col-xs-4 box">
-          <h2><strong>NO MINIMUM<br> ORDER <br> </strong><hr> </h2>
-          <h4>Order any amount<br> you want - no<br> restrictions</h4>
-        </div>
-        <div class="col-xs-4 box">
-          <h2><strong>LIVE ORDER<br> TRACKING <br> </strong><hr> </h2>
-          <h4>Know where your<br> order is at any time</h4>
-        </div>
-        <div class="col-xs-4 box">
-          <h2><strong>SUPERFAST<br> DELIVERY <br> </strong><hr> </h2>
-          <h4>Hot food delivered to<br> your doorstep</h4>
-        </div>
+      <!-- Show this when no user is logged in -->
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> Sign Up <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="customersignup.html">User Sign-up</a></li>
+            <li><a href="managersignup.html">Manager Sign-up</a></li>
+          </ul>
+        </li>
+        <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown"><span class="glyphicon glyphicon-log-in"></span> Login <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="customerlogin.html">User Login</a></li>
+            <li><a href="managerlogin.html">Manager Login</a></li>
+          </ul>
+        </li>
+      </ul>
     </div>
+  </div>
+</nav>
 
-     <div class="col-xs-12 line"><hr></div>
+<!-- Main Content -->
+<div class="wide">
+  <div class="col-xs-5 line"><hr></div>
+  <div class="col-xs-5 line"><hr></div>
+  <div class="tagline"></div>
+</div>
 
-     <div class="paragraph1">
-     <h1>Welcome to  Sea Food Restaurant</h1>
-    <p><br> 
-      <h4>Our Restaurant branches are available at <font color="green"><strong>Shimoga</strong>, <strong>manglore</strong>, <strong>Banglore</strong></font> 
-	   <font color="green"><strong>
-	  </strong>.</font> While someone is asking to eat Fish Fry from <font color="green"><strong>manglore</strong></font>and they just loved it.
-	                                                      THANK YOU FOR VISIT -<font color = "green">arbaazkhan9845@gmai.com
-	 
-    
-    </p>
-    </div>
+<br>
 
-    <div class="col-xs-12 line"><hr></div>
+<div class="orderblock">
+  <h2>Feeling Hungry?</h2>
+  <center><a class="btn btn-success btn-lg" href="customerlogin.html" role="button">Order Now</a></center>
+</div>
 
-    <div class="paragraph1">
-     <h1> Sea Food Restaurant - Good Food is Good Mood</h1>
-    <p><br> 
-      <h4>
-       Sea Food Restaurant will help in making your quest for enjoying good lunch, dinner which is are home delivered all the more interesting and excited when you are scartching your head to entertain your guest with their demands met.
-      </h4>
-    </p>
-    <p>
-      <h4>
-        There are two ways you can <font color="green"><strong>order food online</strong></font> with us. <font color="green"><strong>Download  Sea Food Restaurant app on your smartphone</strong></font> or open our user friendly website and get it home delivered. Or if you are busy in your schedule and just want to eat at leisure at home you can order takeaway from host of restaurants and pickup the food parcel kept ready for you while you are on the way to home.
-      </h4>
-    </p>
-    </di
+<div class="col-xs-12 line"><hr></div>
 
-    <div class="col-xs-12 line"><hr></div>
+<div class="wide2">
+  <div class="col-xs-4 box"><img src="images/minimumx.png" height="200px"></div>
+  <div class="col-xs-4 box"><img src="images/locationx.png"></div>
+  <div class="col-xs-4 box"><img src="images/deliveryx.png"></div>
 
-    <div class="paragraph1">
-          <h1>HOW TO ORDER?</h1>
-          <p>
-            <h4>
-              All it takes is 3 easy steps:
-            </h4>
-          </p>
-          <br>
-          <p>
-            <h3>
-              <span class="glyphicon glyphicon-ok tickicon"></span> <u>SELECT YOUR CITY AND AREA OF DELIVERY</u>
-            </h3>
-          </p>
-          <p>
-            <h4>
-              A list of restaurants is displayed that can deliver your food order to your home.
-            </h4>
-          </p>
-          <br>
-          <p>
-            <h3>
-              <span class="glyphicon glyphicon-ok tickicon"></span> <u>SELECT YOUR RESTAURANT</u>
-            </h3>
-          </p>
-          <p>
-            <h4>
-              You select as per your choice of restaurant from the list serving <font color="green"><strong>cuisines like Indian,south Indian, North Indian,</strong></font> Burgers or Desserts like Ice-cream or Cakes.
-            </h4>
-          </p>
-          <br>
-          <p>
-            <h3>
-              <span class="glyphicon glyphicon-ok tickicon"></span> <u>CHOOSE YOUR DISHES AND PAY ONLINE/OFFLINE</u>
-            </h3>
-          </p>
-          <p>
-            <h4>
-              Once Restaurant is finalised then you browse the restaurants see its reviews, and then Menu.			  Click on the dishes you have been waiting to enjoy and they get added to the cart automatically.
-            </h4>
-          </p>
-          <p>
-            <h4>
-              That’s all once everything is in order hit on the checkout and pays online or cash on delivery,
-			  choose the way you want we accept all major credit cards or pay cash on delivery. <font color="green"><strong>You can order 
-			  food online in cities and areas like Shimoga,Manglore,Banglore.</strong></font>
-            </h4>
-          </p>
-          <br>
-          <p>
-            <h4>
-              Yes,  Sea Food Restaurant is another way to live life with our loved ones...
-            </h4>
-          </p>
-      </div>
+  <div class="col-xs-4 box">
+    <h2><strong>NO MINIMUM<br>ORDER</strong><hr></h2>
+    <h4>Order any amount<br> you want - no<br> restrictions</h4>
+  </div>
+  <div class="col-xs-4 box">
+    <h2><strong>LIVE ORDER<br>TRACKING</strong><hr></h2>
+    <h4>Know where your<br> order is at any time</h4>
+  </div>
+  <div class="col-xs-4 box">
+    <h2><strong>SUPERFAST<br>DELIVERY</strong><hr></h2>
+    <h4>Hot food delivered to<br> your doorstep</h4>
+  </div>
+</div>
 
-        <script type="text/javascript" src="js/jquery.min.js"></script>
-  <script type="text/javascript" src="js/bootstrap.min.js"></script>
-  
+<div class="col-xs-12 line"><hr></div>
+
+<div class="paragraph1">
+  <h1>Welcome to Sea Food Restaurant</h1>
+  <h4>
+    Our Restaurant branches are available at <strong style="color:green">Shimoga</strong>, <strong style="color:green">Mangalore</strong>, <strong style="color:green">Bangalore</strong>.<br>
+    Someone asked for Fish Fry from <strong style="color:green">Mangalore</strong> and they just loved it.<br>
+    THANK YOU FOR VISIT - <strong style="color:green">arbaazkhan9845@gmail.com</strong>
+  </h4>
+</div>
+
+<div class="col-xs-12 line"><hr></div>
+
+<div class="paragraph1">
+  <h1>Sea Food Restaurant - Good Food is Good Mood</h1>
+  <h4>
+    Sea Food Restaurant makes your lunch or dinner exciting with home-delivered meals to entertain guests or yourself.
+  </h4>
+  <h4>
+    You can <strong style="color:green">order food online</strong> via our app or website for delivery or takeaway. Get food ready while you're heading home!
+  </h4>
+</div>
+
+<div class="col-xs-12 line"><hr></div>
+
+<div class="paragraph1">
+  <h1>HOW TO ORDER?</h1>
+  <h4>All it takes is 3 easy steps:</h4>
+
+  <h3><span class="glyphicon glyphicon-ok tickicon"></span> <u>SELECT YOUR CITY AND AREA OF DELIVERY</u></h3>
+  <h4>A list of restaurants is displayed that can deliver to your home.</h4>
+
+  <h3><span class="glyphicon glyphicon-ok tickicon"></span> <u>SELECT YOUR RESTAURANT</u></h3>
+  <h4>Select your favorite restaurant offering cuisines like Indian, South Indian, North Indian, burgers, or desserts.</h4>
+
+  <h3><span class="glyphicon glyphicon-ok tickicon"></span> <u>CHOOSE YOUR DISHES AND PAY ONLINE/OFFLINE</u></h3>
+  <h4>
+    Add dishes to your cart, review your order, and pay with card or cash on delivery.
+    <strong style="color:green">We serve Shimoga, Mangalore, and Bangalore.</strong>
+  </h4>
+
+  <h4>Yes, Sea Food Restaurant is another way to enjoy life with our loved ones...</h4>
+</div>
+
+<!-- JS -->
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+<!-- Footer -->
+<footer class="container-fluid bg-4 text-center">
+  <br>
+  <p>Sea Food Restaurant-2024 | &copy; All Rights Reserved</p>
+  <br>
+</footer>
+
 </body>
-
-  <footer class="container-fluid bg-4 text-center">
-  <br>
-  <p>  Sea Food Restaurant-2024 | &copy All Rights Reserved </p>
-  <br>
-  </footer>
 </html>
